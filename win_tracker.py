4648034cc3bf6cb3ec85b06144a26f0d6bcc852f
iamquoz <46261678+iamquoz@users.noreply.github.com>
2020-08-07 21:32:20 +0400

    Add files via upload

--- win_tracker.py
+++ win_tracker.py
@@ -1,36 +1,124 @@
 import requests
+from os import system, name
 import matplotlib.pyplot as plt
 
-account_id = '' #enter your account ID here
+def conclear(): 
+	# windows 
+	if name == 'nt': 
+		_ = system('cls') 
+	# unix
+	else: 
+		_ = system('clear') 
 
-queries = {'limit': 20, 'lobby_type': 7} #limit is the recent number of matches it will query, lobby type 7 = ranked, other parameters can be added based on openDOTA's API
+conclear()
+
+def check_input(msg, upper_limit):
+	x = input(msg)
+	while (not x.isdigit()) or int(x) > upper_limit:
+		print("\nInput is either not a number or goes above the limit of", upper_limit)
+		x = input(msg)
+	return int(x)
+
+#account_id = int(input("Enter your account ID: ")) 
+
+account_id = check_input("Enter your account ID: ", 1000000000)
+
+queries = {}
+
+
+def change_queries(queries):
+	conclear()
+	print("Would you like to change some of the queries?\n\nOptions include: \n the max amouut of games you'd want to parse, \n patch, \n played hero, \n lobby type")
+	choice = check_input("Enter 1 if you'd want to, enter anything else to not change anything (default is every game you've played): ", 10000000000)
+	if choice != 1:
+		return queries
+	else:
+		while True:
+			choice = check_input("\nEnter 1-4 to change one of the aforementioned queries, 5 to exit: ", 5)
+			if choice == 1: 
+
+				queries["limit"] = check_input("\nEnter the amount of matches you want to parse: ", 100000)
+
+			elif choice == 2: 
+
+				patches_data = requests.get('https://raw.githubusercontent.com/odota/dotaconstants/master/build/patch.json')
+
+				patches = patches_data.json()
+
+				print("Patches and corresponding IDs: \n")
+
+				for patch in patches:
+					if int(patch["id"]) % 20 == 0 and int(patch["id"]) != 0:
+						input ("Next page on input")
+						conclear()
+					print(patch["name"], " - ", patch["id"])
+
+				queries["patch"] = check_input("\nEnter the patch id: ", int(patches[-1]["id"]))
+
+			elif choice == 3: 
+
+				heroes_data = requests.get('https://raw.githubusercontent.com/odota/dotaconstants/master/build/heroes.json')
+
+				heroes = heroes_data.json()
+
+				print("Heroes and corresponding IDs:")
+
+				for hero in heroes:
+					hero_data = heroes[hero]
+					if int(hero) % 20 == 0:
+						input("Next page on input")
+						conclear()
+					print(hero_data["localized_name"], " - ", hero_data["id"])
+
+				queries["hero_id"] = check_input("\nEnter the hero id: ", int(hero_data["id"]))
+
+			elif choice == 4:
+
+				lobbies_data = requests.get('https://raw.githubusercontent.com/odota/dotaconstants/master/build/lobby_type.json')
+
+				lobbies = lobbies_data.json()
+
+				print("Lobbies and corresponding IDs: \n")
+
+				for lobby in lobbies:
+					lobby_info = lobbies[lobby]
+					print(lobby_info["name"], " - ", lobby_info["id"])
+
+				queries["lobby_type"] = check_input("Enter the lobby id: ", int(lobby_info["id"]))
+
+			else:
+				break
+	return queries
+
+change_queries(queries)
 
 res = requests.get(f'https://api.opendota.com/api/players/{account_id}/Matches', params = queries)
 
 data = res.json()
 
 def check_wins(data):
-    match_results = []
-    for matches in data:
-        if matches['radiant_win'] == True and matches['player_slot'] <=127:
-            match_results.append('Win')
-        elif matches['radiant_win'] == False and matches['player_slot'] >127:
-            match_results.append('Win')
-        else:
-            match_results.append('Loss')
-    match_results.reverse() #reverses list to set origin from furthest back requested match
-    return match_results
+	match_results = []
+	for matches in data:
+		if matches['radiant_win'] == True and matches['player_slot'] <= 127:
+			match_results.append('Win')
+		elif matches['radiant_win'] == False and matches['player_slot'] > 127:
+			match_results.append('Win')
+		else:
+			match_results.append('Loss')
+	match_results.reverse() #reverses list to set origin from furthest back requested match
+	return match_results
 
 wins = check_wins(data)
 
-def wins_numerically(match_results): #converts list with history of win loss into plot points for scatter plot +1 for a win, -1 for a loss
-    match_history = [0]
-    for results in match_results:
-        if results == 'Win':
-            match_history.append(match_history[-1]+1)
-        else:
-            match_history.append(match_history[-1] -1)
-    return match_history
+def wins_numerically(match_results): #converts list with history of win loss into plot points for scatter plot +1
+									 #for a win, -1 for a loss
+	match_history = [0]
+	for results in match_results:
+		if results == 'Win':
+			match_history.append(match_history[-1] + 1)
+		else:
+			match_history.append(match_history[-1] - 1)
+	return match_history
 
 results = wins_numerically(wins)
 
